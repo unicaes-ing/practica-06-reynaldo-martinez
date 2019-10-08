65edb2dcfa37cbbdd9e546f5d917093e6f65e0d6
reynaldo martinez <reynaldovalle222@gmail.com>
2019-10-07 20:50:04 -0600

    first commit

---
+++ Practica09Reynadlo/Ejercicio1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica09Reynadlo
+{
+    public class Validacion
+    {
+        public static void Validar(string mensaje, out int num)
+        {
+            bool condicion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(mensaje);
+                condicion = int.TryParse(Console.ReadLine(), out num);
+                if (condicion == false || num < 0)
+                {
+                    Console.WriteLine("Valor no válido. Introduzca nuevamente un valor.");
+                    Console.WriteLine("Presione <Enter> para continuar...");
+                    Console.ReadKey();
+                }
+            } while (condicion == false || num < 0);
+        }
+
+        public static void Validar(string mensaje, double num = 0)
+        {
+            bool condicion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(mensaje);
+                condicion = double.TryParse(Console.ReadLine(), out num);
+                if (condicion == false || num < 0)
+                {
+                    Console.WriteLine("Valor no válido. Introduzca nuevamente un valor.");
+                    Console.WriteLine("Presione <Enter> para continuar...");
+                    Console.ReadKey();
+                }
+            } while (condicion == false || num < 0);
+        }
+
+    }
+
+
+    public class Ejercicio1 : Validacion
+    {
+        public static void Tabla(int num)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num + " * " + i + " = " + num * i);
+            }
+        }
+
+
+        // Funcion2
+        public static void Tabla(int num, ConsoleColor color)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(num + " * " + i + " = " + num * i);
+            }
+        }
+
+    }
+
+    public class Ejercicio2 : Validacion
+    {
+       
+
+
+        #region Funcion1
+        public static void Dibujo(int alto, int ancho)
+        {
+            for (int i = 0; i < alto; i++)
+            {
+                Console.WriteLine(" ");
+                Console.Write(" ");
+                for (int j = 0; j < ancho; j++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+            }
+        }
+        #endregion
+
+        #region Sobrecarga1
+       public static void Dibujo(int alto, int ancho, char caracter)
+        {
+            for (int i = 0; i < alto; i++)
+            {
+                Console.WriteLine(" ");
+                Console.Write(" ");
+                for (int j = 0; j < ancho; j++)
+                {
+                    Console.Write(caracter);
+                    Console.Write(" ");
+                }
+            }
+        }
+        #endregion
+
+        #region Sobrecarga2
+        public static void Dibujo(int alto, int ancho, char caracter, int x, int y)
+        {
+            for (int i = 0; i < alto; i++)
+            {
+                Console.WriteLine(" ");
+                Console.Write(" ");
+                Console.SetCursorPosition(x, y++);
+                for (int j = 0; j < ancho; j++)
+                {
+                    Console.Write(caracter);
+                    Console.Write(" ");
+                }
+            }
+        }
+        #endregion
+    }
+
+    public class Ejercicio3 : Validacion
+    {
+        
+
+        #region Funcion
+        public static int NumElevado(int bases, int exponente)
+        {
+            if (exponente < 1)
+            {
+                return 1;
+            }
+            return bases * NumElevado(bases, exponente - 1);
+        }
+        #endregion
+    }
+
+    public class Ejercicio4
+    {
+        
+
+        public static void DecimalaBinario(int num)
+        {
+            if (num < 2)
+            {
+                Console.Write(num);
+            }
+            else
+            {
+                DecimalaBinario(num / 2);
+                Console.Write(num % 2);
+            }
+        }
+    }
+
+
+}
+
---
+++ Practica09Reynadlo/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica09Reynadlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opc = 0;
+            Console.WriteLine("Coloque un ejercicio 1-4");
+            opc = Convert.ToInt32(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                   int num;
+                   Console.WriteLine("Tabla de multiplicar");
+                   #region LlamadaFuncion
+                   Validacion.Validar("Ingrese el número que desea:", out num);
+                   Ejercicio1.Tabla(num);
+                   Console.WriteLine("Presione <Enter> para continuar...");
+                   Console.ReadKey();
+                   #endregion
+                   Console.Clear();
+                   Console.WriteLine("Ingrese el número que desea:");
+                   Validacion.Validar("Ingrese el número que desea:", out num);
+                   Ejercicio1.Tabla(num, ConsoleColor.Yellow);
+                   Console.WriteLine("Presione <Enter> para continuar...");
+                   Console.ReadKey();
+                    break;
+                case 2:
+                    
+            int ancho, alto, x, y;
+            char caracter;
+            #region Ancho
+            Validacion.Validar("Ingrese el ancho del rectangulo: ", out ancho);
+            #endregion
+            #region Alto
+            Validacion.Validar("Ingrese el alto del rectangulo: ", out alto);
+            #endregion
+            #region Llamada de funcion 1
+            Ejercicio2.Dibujo(ancho, alto);
+            Console.WriteLine("\nPresione <Enter> para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+            // Llamada de la sobrecarga de la función
+            Console.WriteLine("Ingrese el caracter deseado: ");
+            caracter = char.Parse(Console.ReadLine());
+            Ejercicio2.Dibujo(ancho, alto, caracter);
+            Console.WriteLine("\nPresione <Enter> para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            
+            // CoordenadaX
+            Validacion.Validar("Ingrese la coordenada X: ", out x);
+            
+            // CoordenadaY
+            Validacion.Validar("Ingrese la coordenada Y: ", out y);
+            
+             //Llamada de la sobrecarga de la funciòn 2
+            Console.Clear();
+            Ejercicio2.Dibujo(ancho, alto, caracter, x, y);
+            Console.WriteLine("\nPresione <Enter> para continuar...");
+            Console.ReadKey();
+                    break;
+                case 3:
+                 int bases = 0, exponente = 0;
+             //Base
+            Validacion.Validar("Ingrese la base: ", out bases);
+            
+             //Exponente
+            Console.WriteLine();
+            Validacion.Validar("Ingrese el exponente: ", out exponente);
+            
+            Console.WriteLine(Ejercicio3.NumElevado(2, 4));
+            Console.WriteLine("Presione <Enter> para continuar...");
+            Console.ReadKey();
+                    break;
+                case 4:
+                    int n = 0;
+            Console.WriteLine("Ingrese el numero");
+            n = Convert.ToInt32(Console.ReadLine());
+            Ejercicio4.DecimalaBinario(n);
+            Console.WriteLine("\nPresione <Enter> para continuar...");
+            Console.ReadKey();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
